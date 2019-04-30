ea4d130b715cfb3797f20cd3a99b53203fac9104
dufei <dufei@daling.com>
2019-04-30 16:26:53 +0800

    基础样式

---
+++ src/utils/baseStyle.js
@@ -0,0 +1,35 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+// #F6AA00
+
+// 设置全局默认主题
+export default createMuiTheme({
+    palette: {
+        primary: {
+            main: '#F6AA00', //主要样式
+            // light: '#757ce8', 
+            // dark: '#002884',
+            contrastText: '#fff',
+        },
+        secondary: green,
+        text: {
+            disabled: "#F6AA00",
+            hint: "#F6AA00",
+            primary: "#F6AA00",
+            secondary: "#F6AA00",
+        },
+    },
+
+    status: {
+        danger: 'orange',
+    },
+    typography: {
+        useNextVariants: true,
+    },
+
+}
+
+
+);
+
+
